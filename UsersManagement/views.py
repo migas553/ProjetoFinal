c2c357138b0bc3b6f4e48686ad4906d885dd4026
Migas553 <migas553@gmail.com>
2023-12-16 23:29:01 +0000

    Primeiro teste de registo de utilizadores

--- UsersManagement/views.py
+++ UsersManagement/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth import login
 
 # Create your views here.
+def SignUp(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form=SignUpForm()
+    return render(request, 'UsersManagement/signup.html', {'form' : form})
+
+# Frontpage temporaria
+def frontpage(request):    
+    return render(request, 'UsersManagement/frontpage.html')

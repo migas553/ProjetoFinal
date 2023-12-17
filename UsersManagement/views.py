731b56dad6deb2718595ee1626779a60aa9ff94a
Migas553 <migas553@gmail.com>
2023-12-17 18:00:01 +0000

    Pagina my account

--- UsersManagement/views.py
+++ UsersManagement/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
 from django.contrib.auth import login
-
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 def SignUp(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -18,3 +17,23 @@ def SignUp(request):
 # Frontpage temporaria
 def frontpage(request):    
     return render(request, 'UsersManagement/frontpage.html')
+
+
+@login_required
+def my_account(request):
+    return render(request, 'UsersManagement/my_account.html')
+
+
+
+@login_required
+def edit_my_account(request):
+    if request.method =='POST':
+        user = request.user
+        user.first_name = request.POST.get('first_name')
+        user.last_name = request.POST.get('last_name')
+        user.username = request.POST.get('username')
+        user.email = request.POST.get('email')
+        user.save()
+        return redirect('my_account')
+        
+    return render(request, 'UsersManagement/edit_my_account.html')

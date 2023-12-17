9c24530751eaf828d75c7d6b48b4e61899f52cbe
Migas553 <migas553@gmail.com>
2023-12-17 01:21:00 +0000

    Login a funcionar

--- UsersManagement/urls.py
+++ UsersManagement/urls.py
@@ -1,8 +1,10 @@
-
+from django.contrib.auth.views import LoginView,LogoutView
 from django.urls import path
 from .views import *
 urlpatterns = [
     path('',frontpage, name='frontpage'),
     path('signup/', SignUp, name='signup' ),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout' ),
+    path('login/', LoginView.as_view(template_name='UsersManagement/login.html',next_page='/'), name='login' ),
     
 ]

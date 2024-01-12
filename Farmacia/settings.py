551727682a7706de87dc1382fd8eb2af69950b60
migas553 <migas553@gmail.com>
2024-01-12 10:03:00 +0000

    media files fixed

--- Farmacia/settings.py
+++ Farmacia/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'Orders.context_processors.cart_count',
+                'django.template.context_processors.media',
             ],
         },
     },
